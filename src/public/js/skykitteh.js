f4aa29190efd49010369ad4db0d4e9d82f19043e
Gardeners personliga blogg <gardener@Balthazar.(none)>
2011-04-11 21:26:53 +0200

    yikes, missystaaaaake!

---
+++ src/public/js/skykitteh.js
@@ -0,0 +1,45 @@
+// SkyKitteh javascript
+
+$(document).ready(function() {
+
+  // Automatically scroll page to given element.
+  function scrollTo(sel) {
+    var destination = $(sel).offset().top;
+    $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-20}, 500 );
+  }
+
+  function setupToggleButton(btn, el) {
+    $(btn).click(function() {
+      $(el).toggle();
+      scrollTo(el);
+    });
+  }
+
+  // Setup nyroModal
+  $(function() {
+    $('.nyroModal').nyroModal();
+  });
+
+  // Hide all functionality
+  $('#skykitteh-view').hide();
+  $('#skykitteh-files').hide();
+  $('#skykitteh-upload').hide();
+  $('#skykitteh-modules').hide();
+  $('#skykitteh-edit').hide();
+  if (document.location.toString().match(/skykitteh.com\/edit\//)) {
+    $.nmManual('#skykitteh-edit');
+  }
+
+  // Handle create file button
+  $('#newfile').click(function() {
+    jPrompt('Dah naem of new code nomz:', '', 'Can haz nomz?', function(filename) {
+      if (filename) {
+        document.location = '/edit/' + filename;
+      }
+    });
+  });
+
+  // Setup tabby on textarea to allow tabbing
+  $('#skykitteh-editcode').tabby();
+
+});
