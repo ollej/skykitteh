65ffcd8d02e2ca4da57a9704d32f706dbf003a05
Gardeners personliga blogg <gardener@Balthazar.(none)>
2011-04-20 23:12:10 +0200

    I can haz IO?

---
+++ src/lib/SkyChat.pm
@@ -0,0 +1,45 @@
+package SkyChat;
+
+use strict;
+use warnings;
+
+my %users = ();
+
+sub login
+{
+    my ($user) = @_;
+
+    $users{$user} = [] unless($user eq 'skykitteh');
+}
+
+sub logout
+{
+    my ($user) = @_;
+    delete $users{$user};
+
+    broadcast('skykitteh', $user.' saiz no moar.')
+}
+
+sub broadcast
+{
+    my ($user, $msg) = @_;
+
+    foreach my $u (keys %users)
+    {
+	push @{$users{$u}}, [$user, $msg];
+    }
+}
+
+sub poll
+{
+    my ($user) = @_;
+
+    my $queue = $users{$user};
+    return [] unless ($queue);
+
+    $users{$user} = [];
+
+    return $queue;
+}
+
+1;
