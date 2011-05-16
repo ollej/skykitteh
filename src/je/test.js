5968f6e670c400bbfdad6d405dd1e5fe9ad1df29
Gardeners personliga blogg <gardener@Balthazar.(none)>
2011-05-16 19:05:14 +0200

    Test JS.

--- src/je/test.js
+++ src/je/test.js
@@ -1,1 +1,14 @@
-return { "template": "error", "stash": {"msg":"move along."} };
+function render(template, vars)
+{
+    document.mojo.stash(vars);
+    document.mojo.render({'template': template});
+}
+
+var test = document.mojo.param('test');
+var vars = {
+    'msg': 'move along.'
+};
+
+vars['msg'] += ' (param "test" is "'+test+'")';
+
+render('error', vars);
